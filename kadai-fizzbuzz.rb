3e02611b3ecd5dba26227d4a2f4b3891a008b919
taki-tonoma <tonoma@i.softbank.jp>
2021-08-07 06:30:22 +0000

    second commit

--- kadai-fizzbuzz.rb
+++ kadai-fizzbuzz.rb
@@ -1,13 +1,13 @@
 
 def fizzbuzz(num)
   if (num % 3 == 0) && (num % 5 == 0)
-    puts 'FizzBuzz'
+    return 'FizzBuzz'
   elsif num % 3 == 0
-    puts 'Fizz'
+    return 'Fizz'
   elsif num % 5 == 0
-    puts 'Buzz'
+    return 'Buzz'
   else
-    puts num
+    return num
   end
 end
 
@@ -15,7 +15,7 @@ i = 1
 num_max = 100
 
 while i < num_max
-  fizzbuzz(i)
+  puts fizzbuzz(i)
   i+= 1
 end
 
